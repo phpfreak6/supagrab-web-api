ee5797dd5af8bc3ae658abfbe8261ac3edaa8e59
php freak <php.freak6@gmail.com>
2021-07-14 17:32:27 +0530

    Added USER_IMAGE_UPLOAD_PATH and USER_IMAGE_BASE_PATH in .env

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     mongodbUrl: process.env.MONGODB_URL,
     basePath: process.env.BASE_URL,
-    userImageUploadPath: 'public/images/uploads/users',
-    userImageBasePath: process.env.BASE_URL + 'images/uploads/users',
+    userImageUploadPath: process.env.USER_IMAGE_UPLOAD_PATH,
+    userImageBasePath: process.env.BASE_URL + process.env.USER_IMAGE_BASE_PATH,
     JWT_SECRET: process.env.JWT_SECRET_KEY
 }
