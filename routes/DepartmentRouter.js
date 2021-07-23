94e1f1d46ec960ff0c08e56dcc64d4e9c6bccaf6
php freak <php.freak6@gmail.com>
2021-07-23 18:40:52 +0530

    intermediate commit

--- routes/DepartmentRouter.js
+++ routes/DepartmentRouter.js
@@ -4,12 +4,44 @@ const router = express.Router();
 const DepartmentController = new require('../controllers').DepartmentController;
 const DepartmentControllerObj = new DepartmentController();
 
+let DEPARTMENT_IMAGE_PATH = require('../config/config').DEPARTMENT_IMAGE_PATH;
+/**
+ * IMAGE UPLOAD STARTS
+ */
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, userImagePath)
+    },
+    filename: function (req, file, cb) {
+        let id = req.params.id;
+        let originalname = file.originalname;
+        let newFileName = id;
+        let extention = path.extname(originalname);
+        let fullFileName = newFileName + extention;
+        let fullFileNameWithPath = userImagePath + '/' + fullFileName;
+        req.params.imageDetails = {
+            fileOriginalname: originalname,
+            newFileName: newFileName,
+            fileExtention: extention,
+            fullFileName: fullFileName,
+            fullFileNameWithPath: fullFileNameWithPath
+        };
+        cb(null, fullFileName);
+    }
+});
+
+const upload = multer({storage: storage, limits: {fileSize: 1000000}});
+let cpUpload = upload.fields([{name: 'profile_pic', maxCount: 1}]);
+
 router.get('/exists/:title/:id?', [DepartmentControllerObj.exists]);
 router.get('/', [DepartmentControllerObj.get]);
 router.get('/:id', [DepartmentControllerObj.getById]);
 router.post('/', [DepartmentControllerObj.insert]);
 router.patch('/:id', [DepartmentControllerObj.update]);
 router.delete('/:id', [DepartmentControllerObj.delete]);
-router.post('image/:id', [DepartmentControllerObj.uploadImage]);
+router.post('image/:id', cpUpload, [DepartmentControllerObj.uploadImage]);
 
 module.exports = router;
