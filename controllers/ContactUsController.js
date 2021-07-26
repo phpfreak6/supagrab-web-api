efe43c92dea13b91616b7ee36fb81093c9a8100a
php freak <php.freak6@gmail.com>
2021-07-26 17:41:56 +0530

    Additional changes

--- controllers/ContactUsController.js
+++ controllers/ContactUsController.js
@@ -19,7 +19,7 @@ module.exports = class ContactUsController {
     async contact(req, res, next) {
         try {
             let data = req.body;
-            let rules = {first_name: 'required', email: 'required', message: 'required'};
+            let rules = {first_name: 'required', email: 'required|email', message: 'required'};
             let validation = new Validator(data, rules);
             if (validation.fails()) {
                 return responseServiceObj.sendException(res, {
@@ -32,7 +32,7 @@ module.exports = class ContactUsController {
             MailServiceObj.sendMail({to: ADMIN_EMAIL, html: admin_email_html, subject: 'New Query'}).then(async (result) => {
                 if (result.sent == false) {
                     console.log('Contact Us Admin Email Failed');
-                    FailedMailServiceObj.insertFailedMail({email: result.data.to, data: result.data, type: 'CONTACT_US_ADMIN'});
+                    await FailedMailServiceObj.insertFailedMail({email: result.data.to, data: result.data, type: 'CONTACT_US_ADMIN'});
                 }
             }).catch(async (ex) => {
                 console.log(ex);
@@ -43,7 +43,7 @@ module.exports = class ContactUsController {
             MailServiceObj.sendMail({to: data.email, html: customer_thankyou_html, subject: 'Thankyou For Writing To Us'}).then(async (result) => {
                 if (result.sent == false) {
                     console.log('Customer Thankyou Mail Failed');
-                    FailedMailServiceObj.insertFailedMail({email: result.data.to, data: result.data, type: 'CONTACT_US_ENQUIRER'});
+                    await FailedMailServiceObj.insertFailedMail({email: result.data.to, data: result.data, type: 'CONTACT_US_ENQUIRER'});
                 }
             }).catch(async (ex) => {
                 console.log(ex);
