f9c465cbbc1598b50f82c854efe92bef70b82913
DESKTOP-HLB995O\immi <php.freak6@gmail.com>
2021-08-13 09:34:36 +0530

    .env updated

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -12,34 +12,34 @@ const path = require('path');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, ImagePath)
-  },
-  filename: function (req, file, cb) {
-    let id = req.params.id;
-    let originalname = file.originalname;
-    let newFileName = id;
-    let extention = path.extname(originalname);
-    let fullFileName = newFileName + extention;
-    let fullFileNameWithPath = ImagePath + '/' + fullFileName;
-    req.params.imageDetails = {
-      fileOriginalname: originalname,
-      newFileName: newFileName,
-      fileExtention: extention,
-      fullFileName: fullFileName,
-      fullFileNameWithPath: fullFileNameWithPath
-    };
-    cb(null, fullFileName);
-  }
+	destination: function (req, file, cb) {
+		cb(null, ImagePath)
+	},
+	filename: function (req, file, cb) {
+		let id = req.params.id;
+		let originalname = file.originalname;
+		let newFileName = id;
+		let extention = path.extname(originalname);
+		let fullFileName = newFileName + extention;
+		let fullFileNameWithPath = ImagePath + '/' + fullFileName;
+		req.params.imageDetails = {
+			fileOriginalname: originalname,
+			newFileName: newFileName,
+			fileExtention: extention,
+			fullFileName: fullFileName,
+			fullFileNameWithPath: fullFileNameWithPath
+		};
+		cb(null, fullFileName);
+	}
 });
 
 const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 } // (1000000 bytes = 1MB)
+	storage: storage,
+	limits: { fileSize: 1000000 } // (1000000 bytes = 1MB)
 });
 
 var cpUpload = upload.fields([
-  { name: 'profile_pic', maxCount: 1 }
+	{ name: 'profile_pic', maxCount: 1 }
 ]);
 /**
  * IMAGE UPLOAD ENDS
@@ -54,7 +54,7 @@ var cpUpload = upload.fields([
 // ]);
 
 router.patch('/setStatus/:id', [
-  ProductControllerObj.setStatus
+	ProductControllerObj.setStatus
 ]);
 
 // router.post('/changePic/:id', cpUpload, [
@@ -62,39 +62,39 @@ router.patch('/setStatus/:id', [
 // ]);
 
 router.get('/byDepartment/:department_slug', [
-  ProductControllerObj.productByDepartment
+	ProductControllerObj.productByDepartment
 ]);
 
 router.get('/byCategory/:category_slug', [
-  ProductControllerObj.productByCategory
+	ProductControllerObj.productByCategory
 ]);
 
 router.get('/product-slug-exists', [
-    ProductControllerObj.slugExists
+	ProductControllerObj.slugExists
 ]);
 
 router.get('/product-exists', [
-    ProductControllerObj.exists
+	ProductControllerObj.exists
 ]);
 
 router.patch('/:id', [
-  ProductControllerObj.update
+	ProductControllerObj.update
 ]);
 
 router.delete('/:id', [
-  ProductControllerObj.delete
+	ProductControllerObj.delete
 ]);
 
 router.get('/:id', [
-  ProductControllerObj.getById
+	ProductControllerObj.getById
 ]);
 
 router.post('/', [
-  ProductControllerObj.insert
+	ProductControllerObj.insert
 ]);
 
 router.get('/', [
-  ProductControllerObj.get
+	ProductControllerObj.get
 ]);
 
 /**
